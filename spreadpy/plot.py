db704797c6a2253a86711444b5a123dd602e5185
Sebastian <sebaseba12@gmail.com>
2023-08-10 14:20:11 -0400

    Update plot.py

--- spreadpy/plot.py
+++ spreadpy/plot.py
@@ -16,5 +16,15 @@ def animate(fig, ax, graph, vertex_colors, layout, plot_kwargs={},
     graph_plot = ig.plot(graph, target=ax, layout=layout,
                          vertex_color=vertex_colors[0], **plot_kwargs)
 
+<<<<<<< Updated upstream
     ani = animation.FuncAnimation(fig, _update_graph, **animation_kwargs)
     ani.save(**saving_kwargs)
+=======
+    ani = animation.FuncAnimation(fig, _update_graph,
+                                  frames, interval=interval,
+                                  blit=blit, **animation_kwargs)
+    ani.save(save_as, writer=writer, fps=fps, **saving_kwargs)
+
+def plot(fig, ax):
+    pass
+>>>>>>> Stashed changes
